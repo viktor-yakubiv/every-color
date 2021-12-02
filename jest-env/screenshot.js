d1b730f7993fe6f0848be9279d2171749858b31e
Viktor Yakubiv <viktor@yakubiv.com>
2021-12-02 22:56:06 +0200

    Fix screenshot taking functionality 🐛
    Path was not resolving from the project root.
    Also, ignore rule for the screenshot directory added.

--- jest-env/screenshot.js
+++ jest-env/screenshot.js
@@ -1,7 +1,14 @@
-const takeScreenshot = async (name = expect.getState().currentTestName) =>
-  page.screenshot({
-    path: `${__dirname}/screenshots/${name}.png`,
+import { promises as fs } from 'fs'
+import path from 'path'
+
+const screenshotRoot = `${path.join(__dirname, '../')}/screenshots`
+
+const takeScreenshot = async (name = expect.getState().currentTestName) => {
+  await fs.mkdir(screenshotRoot, { recursive: true })
+  await page.screenshot({
+    path: `${screenshotRoot}/${name}.png`,
     fullPage: true,
   })
+}
 
 export default takeScreenshot
