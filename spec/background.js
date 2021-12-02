2e0bd2cd76ff0bc944e88a10fdc8606ebbcff1a1
Viktor Yakubiv <viktor@yakubiv.com>
2021-12-02 23:17:13 +0200

    Improve background test ✅

--- spec/background.js
+++ spec/background.js
@@ -1,7 +1,21 @@
-test('page background is the one defined by the theme', async () => {
-  await injectPage('<h1>Background</h1>bit more stuff')
-
+test.only('page background is the one defined by the theme', async () => {
+  await injectPage()
   await expect(styleOf(':root')).toMatchStyle({
     backgroundColor: 'var(--theme-background)',
   })
+  await takeScreenshot('background-default')
+
+  // Modifying background to be able clearly see the variable is applied
+  await injectPage(`
+    <style>
+      :root {
+        --theme-background: #212121;
+        color: #fafafa !important; /* overriding theme to make text readable */
+      }
+    </style>
+  `)
+  await expect(styleOf(':root')).toMatchStyle({
+    backgroundColor: '#212121',
+  })
+  await takeScreenshot('background-modified')
 })
